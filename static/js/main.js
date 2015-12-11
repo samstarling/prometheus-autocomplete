4878dd07d0060452186993f9daea9b3d7be05f56
Sam Starling <sam@soundcloud.com>
2015-12-11 12:23:32 +0100

    Add example with no minimum length

--- static/js/main.js
+++ static/js/main.js
@@ -27,5 +27,18 @@ $(document).ready(function() {
       limit: 10,
       data: data.data
     });
+
+    // Example 1.3
+    var $inputor = $('#example-1-3').atwho({
+      at: "",
+      highlightFirst: false,
+      minLen: 0,
+      maxLen: 300,
+      insertTpl: "${name}",
+      suffix: "",
+      startWithSpace: false,
+      limit: 10,
+      data: data.data
+    });
   });
 });
