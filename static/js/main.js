2cfee0da17236caf5fc1285644f32841f0600d22
Sam Starling <sam@soundcloud.com>
2015-12-11 12:16:19 +0100

    Fix typo which prevented @ from working

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  
+
   $.get('data/example.json', function(data) {
-    
+
     // Example 1.1
-    var $inputor = $('#example-1-2').atwho({
+    var $inputor = $('#example-1-1').atwho({
       at: "@",
       highlightFirst: false,
       minLen: 0,
@@ -14,7 +14,7 @@ $(document).ready(function() {
       limit: 10,
       data: data.data
     });
-    
+
     // Example 1.2
     var $inputor = $('#example-1-2').atwho({
       at: "",
